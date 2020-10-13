12e55cbc1a95ea48b51cf1082737519454731514
llabuzie <llabuzie@iu.edu>
2020-10-12 22:31:28 -0400

    kept correct app.js and app.css

--- src/app.js
+++ src/app.js
@@ -1,26 +1,34 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import SignInSide from "./Pages/SignInSide";
-import SignUp from "./Pages/SignUp";
-import Home from "./Pages/Home";
-import Navbar from "./components/Navbar";
+import { Route, Switch } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { useAuth0 } from "@auth0/auth0-react";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { NavBar, Footer, Loading, PrivateRoute } from "./components";
+import { Home, Profile, AboutUs, ContactUs } from "./views";
 
-function App() {
+import "./app.css";
+
+const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <main>
-      <Navbar />
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/sign-in" component={SignInSide} />
-        <Route path="/sign-up" component={SignUp} />
-
-        <Route component={Error} />
-      </Switch>
-    </main>
+    <div id="app" className="d-flex flex-column h-100">
+      <NavBar />
+      <Container className="flex-grow-1 mt-5">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <PrivateRoute path="/profile" component={Profile} />
+          <Route path="/about-us" exact component={AboutUs} />
+          <Route path="/contact-us" exact component={ContactUs} />
+        </Switch>
+      </Container>
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
