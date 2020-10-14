3bc0b7602e36efe1c60b2350dc6728de0b87fbde
llabuzie <llabuzie@iu.edu>
2020-10-13 22:22:33 -0400

    added more starter files for manager view

--- src/app.js
+++ src/app.js
@@ -4,9 +4,14 @@ import { Container } from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import { NavBar, Footer, Loading, PrivateRoute } from "./components";
-import { Home, Profile, AboutUs, ContactUs, ManDashboard } from "./views";
-
+import { Home, Profile, AboutUs, ContactUs, ManDashboard, ViewData } from "./views";
+import AddSnack from "./views/manager/addSnack";
+import RemoveSnack from "./views/manager/removeSnack";
+import AddShowing from "./views/manager/addShowing";
+import RemoveShowing from "./views/manager/removeShowing";
+import EditTheater from "./views/manager/editTheaterInfo";
 import "./app.css";
+
 
 const App = () => {
   const { isLoading } = useAuth0();
@@ -25,6 +30,12 @@ const App = () => {
           <Route path="/about-us" exact component={AboutUs} />
           <Route path="/contact-us" exact component={ContactUs} />
           <Route path="/manager/dashboard" exact component={ManDashboard} />
+          <Route path="/manager/addSnack" exact component={AddSnack} />
+          <Route path="/manager/removeSnack" exact component={RemoveSnack} />
+          <Route path="/manager/addShwoing" exact component={AddShowing} />
+          <Route path="/manager/removeShowing" exact component={RemoveShowing} />
+          <Route path="/manager/editTheaterInfo" exact component={EditTheater} />
+          <Route path="/manager/viewData" exact component={ViewData} />
         </Switch>
       </Container>
       <Footer />
---
+++ src/views/manager/removeSnack.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+
+
+const RemoveSnack = () => {
+  
+  return (
+    <Container className="mb-5">
+      <Row className="align-items-center profile-header mb-5 text-center text-md-left"></Row>
+      <h1>Manager Dashboard</h1>
+      
+    </Container>
+  );
+};
+
+export default RemoveSnack;
